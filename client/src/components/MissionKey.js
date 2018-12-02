716b7b525d8059cae08f78819ec8e3b060fba841
alexojin <aojin91@gmail.com>
2018-12-02 16:58:19 -0600

    Ready To Deploy
    React Apollo Website with SpaceX GraphQL queries

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MissionKey() {
+  return (
+    <div>
+      <div className='my-3'>
+        <p>
+          <span className='px-3 mr-2 bg-success' /> = Success
+        </p>
+        <p>
+          <span className='px-3 mr-2 bg-danger' /> = Failure
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MissionKey;
